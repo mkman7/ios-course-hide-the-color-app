fa31b325cb2891a9df00222e0fec668ed37ba31c
mustafa akman <Mustafa@mustafas-mac.local>
2016-06-16 11:31:02 +0300

    xcode projemi commit ettim

--- HideTheColors/ViewController.swift
+++ HideTheColors/ViewController.swift
@@ -10,6 +10,14 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var kırmızı: UIImageView!
+    @IBOutlet weak var mavi: UIImageView!
+    @IBOutlet weak var gizleKırmızı: UIButton!
+    @IBOutlet weak var gizleMavi: UIButton!
+    @IBOutlet weak var gösterKırmızı: UIButton!
+    @IBOutlet weak var gösterMavi: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +28,19 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func MaviyiGizle(sender: AnyObject) {
+        mavi.hidden = true
+    }
+    @IBAction func KırmızıyıGizle(sender: AnyObject) {
+        kırmızı.hidden = true
+    }
+    @IBAction func KırmızıyıGöster(sender:AnyObject) {
+        kırmızı.hidden = false
+    }
+    @IBAction func MaviyiGöster(sender: AnyObject) {
+        mavi.hidden = false
+    }
+    
 
 }
 
